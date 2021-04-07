fb90dd390097fd9545be5842e56f464652800dd2
Eric <lee.erictae@gmail.com>
2021-04-07 18:46:52 -0500

    install bootstrap, add index view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <p> {{product.name }}</p>
+
+    </div>
   </div>
 </template>
 
@@ -8,13 +12,26 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      console.log("in products index...");
+      axios.get("http://localhost:3000/api/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
