0209ef5eabd46ed9d2dc61e70338df27fd1b2987
Eric <lee.erictae@gmail.com>
2021-04-07 20:05:12 -0500

    add nav, card style, create productindex.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/ProductsIndex.vue'
+import ProductsIndex from '../views/ProductsIndex.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,12 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/products',
+    name: 'ProductsIndex',
+    component: ProductsIndex
+  },
+
   {
     path: '/about',
     name: 'About',
--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <div v-for="product in products">
-      <p> {{product.name }}</p>
-      
+    <br />
+  <div class="row">
+  <div class="col-sm-4" v-for="product in products">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
 
     </div>
   </div>
